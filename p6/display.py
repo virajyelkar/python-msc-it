8e9ad58523ccd689d0c4104503e44e5d4cc2137d
virajyelkar <virajyelkar@gmail.com>
2026-08-19 14:43:46 +0530

    Add files via upload

---
+++ p6/display.py
@@ -0,0 +1,5 @@
+def display_data():
+    # Display
+    print("\nRank  Roll  Name  Total  Percentage  Grade")
+    for s in students:
+        print(s[5], s[0], s[1], s[2], s[3], s[4])
---
+++ p6/student_data.py
@@ -0,0 +1,30 @@
+def get_student_data():
+    n = int(input("Enter number of students: "))
+
+    students = []
+
+    for i in range(n):
+        roll = input("Enter Roll No: ")
+        name = input("Enter Name: ")
+
+        marks = []
+        for j in range(5):
+            marks.append(int(input("Enter marks: ")))
+
+        total = sum(marks)
+        percentage = total / 5
+
+        if percentage >= 90:
+            grade = "A+"
+        elif percentage >= 80:
+            grade = "A"
+        elif percentage >= 70:
+            grade = "B"
+        elif percentage >= 60:
+            grade = "C"
+        elif percentage >= 50:
+            grade = "D"
+        else:
+            grade = "F"
+
+        students.append([roll, name, total, percentage, grade])
